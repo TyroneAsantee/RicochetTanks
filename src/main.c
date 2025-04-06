041d2a20ac29149fa40f91d76789b94e71e68fe2
XxNitXZ <nitin94847@gmail.com>
2025-04-06 20:50:12 +0200

    Lägger till små saker

--- src/main.c
+++ src/main.c
@@ -3,6 +3,9 @@
 #include <SDL_image.h>
 #include <stdbool.h>
 
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
 int main(int argv, char** args){
     if(SDL_Init(SDL_INIT_VIDEO)!=0){
         printf("Error: %s\n",SDL_GetError());
