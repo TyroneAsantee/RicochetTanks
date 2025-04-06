51b055465e0522839031d7ef6f53acd7c082eb9b
XxNitXZ <nitin94847@gmail.com>
2025-04-06 20:01:24 +0200

    Lade till SDL_image.h för bildhantering

--- src/main.c
+++ src/main.c
@@ -1,3 +1,10 @@
 #include <stdio.h>
 #include <SDL.h>
 #include <SDL_image.h>
+
+int main(int argv, char** args){
+    if(SDL_Init(SDL_INIT_VIDEO)!=0){
+        printf("Error: %s\n",SDL_GetError());
+        return 1;
+    }
+}
